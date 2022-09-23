fe9850789ee9aa4c862ffd26d2f3d0f647857bd8
Vincent Basile <vpbphone@gmail.com>
2022-09-23 19:55:58 -0400

    Blank Router App

--- src/pages/PageTwo/index.tsx
+++ src/pages/PageTwo/index.tsx
@@ -1,4 +1,3 @@
-import logo from '../../logo.svg';
 import './style.css';
 import { Link } from 'react-router-dom';
 
@@ -6,7 +5,6 @@ export function PageTwo() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
